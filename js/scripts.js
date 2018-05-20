e6fc957d97641921df2ef448f1817b7e733fb41c
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-21 00:59:19 +0300

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,24 @@ $(document).ready(function () {
     $("button#noSkip").click(function () {
         $("button#skip").hide();
         $("div#landing").fadeOut(2000);
-        $("div#hi").delay(3000).fadeIn(2000);
-        $("div#hi").fadeOut(3000);
-        $("div#time").delay(8000).fadeIn(2000);
+        $("div#hi").delay(1000).fadeIn(2000);
+        $("div#hi").fadeOut(1000);
+        $("div#time").delay(4000).fadeIn(2000);
         $("div#time").fadeOut(2000);
-        $("div#begin").delay(13000).fadeIn(2000);
+        $("div#begin").delay(10000).fadeIn(2000);
         $("div#begin").fadeOut(2000);
-        $("div#intro").delay(18000).fadeIn(2000);
-        $("p#intro-cont").delay(21000).fadeIn(2000);
+        $("div#intro").delay(13000).fadeIn(2000);
+        $("p#intro-cont").delay(15000).fadeIn(2000);
         $("div#intro").delay(4000).fadeOut(2000);
-        $("div#intro-story").delay(29000).fadeIn(2000);
+        $("div#intro-story").delay(15000).fadeIn(2000);
         $("div#intro-story").delay(2000).fadeOut(2000);
-        $("div#intro-story1").delay(36000).fadeIn(2000);
+        $("div#intro-story1").delay(23000).fadeIn(2000);
         $("div#intro-story1").fadeOut(2000);
-        $("div#intro-story2").delay(41000).fadeIn(2000);
+        $("div#intro-story2").delay(32000).fadeIn(2000);
         $("div#intro-story2").delay(2000).fadeOut(2000);
-        $("div#intro-story3").delay(48000).fadeIn(2000);
+        $("div#intro-story3").delay(32000).fadeIn(2000);
         $("div#intro-story3").fadeOut(2000);
-        $("div#welcome").delay(53000).fadeIn(2000);
+        $("div#welcome").delay(32000).fadeIn(2000);
         $("button#enter").delay(56000).fadeIn(2000);
     });
     $("button#enter").click(function () {
